3d3ad0d7f076ca9de30860867fa56a2e8d877a97
mete0r <mete0r@sarangbang.or.kr>
2014-05-03 05:42:20 +0900

    redirect cli.main() to gtk2.main()

--- MYAPP/cli.py
+++ MYAPP/cli.py
@@ -16,7 +16,8 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+from MYAPP.gtk2 import main as gtk2app_main
 
 
 def main():
-    pass
+    return gtk2app_main()
