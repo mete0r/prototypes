5d0ceaf4dd96cfdc4073599d545b272eb00805ac
mete0r <mete0r@sarangbang.or.kr>
2015-11-27 00:44:10 +0900

    .gae.mailer

---
+++ MYAPP/gae/mailer.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+#   MYAPP : SOME_DESCRIPTION
+#   Copyright (C) 2015 mete0r <mete0r@sarangbang.or.kr>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import logging
+
+from zope.interface import implementer
+
+from ..interfaces import IMailer
+
+
+logger = logging.getLogger(__name__)
+
+
+@implementer(IMailer)
+class Mailer(object):
+
+    def send(self, mime_message):
+        from google.appengine.api.mail import EmailMessage
+        logger.debug('%s', mime_message.as_string())
+        message = EmailMessage(mime_message)
+        message.send()
