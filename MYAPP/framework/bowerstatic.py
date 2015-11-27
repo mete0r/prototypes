30fceab9a06ad8a7601910012f91b40269e693a9
mete0r <mete0r@sarangbang.or.kr>
2015-11-27 20:06:11 +0900

    .framework.bowerstatic

---
+++ MYAPP/framework/bowerstatic.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+#   MYAPP : SOME_DESCRIPTION
+#   Copyright (C) 2015 mete0r <mete0r@sarangbang.or.kr>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from zope.interface import Interface
+import bowerstatic
+
+
+class IBower(Interface):
+    ''' Marker interface '''
+
+
+class IBowerComponents(Interface):
+    ''' Marker interface '''
+
+
+bower = bowerstatic.Bower()
+
+
+def includeme(config):
+    config.registry.registerUtility(bower, IBower)
+    config.add_tween(__name__ + '.tween_factory')
+
+
+def tween_factory(handler, registry):
+    injector = bowerstatic.InjectorTween(bower, handler)
+    publisher = bowerstatic.PublisherTween(bower, injector)
+    return publisher
