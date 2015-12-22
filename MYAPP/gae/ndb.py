4b3391df0d94f7bf2a949a16ff4c842ed587871d
mete0r <mete0r@sarangbang.or.kr>
2015-12-22 19:12:00 +0900

    .gae.ndb: new model states

--- MYAPP/gae/ndb.py
+++ MYAPP/gae/ndb.py
@@ -18,52 +18,86 @@
 #
 '''
 
-Model states:
+==> strong reference
+--> weak reference
 
-1. Free
+Session ====> Model state ====> Model
+Session <---- Model state <---- Model
 
-If a Model is not stored in the DataStore, has no key and is not associated
-with a Session, it's Free.
 
-Associate raises.
+
+Entity states
+-------------
+
+0. Unknown
+
+If an Entry is not in a Session, it's Unknown.
+
+Unknown --getitem--> Absent
+Unknown --getitem--> Persistent Clean
+Unknown --setitem--> New Identified (an COMPLETE key will be assigned)
+Unknown --add--> New Unidentified (an INCOMPLETE key will be assigned)
+
+or else it's Known.
+
+1. Absent
+
+If an Entity is not stored in the DataStore nor in the Session, it's Absent.
+
+Absent --setitem-> New Identified (an COMPLETE key will be assigned)
 
 2. New
 
-If a Model is not stored in the DataStore but has a key, then but is not
-associated with a Session, it's New.
+If a Model is not stored in the DataStore but is associated with a Session,
+it's New.
 
-Associate to Pending.
+If its key is ABSENT or INCOMPLETE, it's New Unidentified.
+If its key is COMPLETE, it's New Identified.
 
-3. Pending
+New Unidentified --flush-> Persistent Clean (key will become COMPLETE)
+New Unidentified --remove-> Absent
+New Identified --delitem-> Absent
+New Identified --flush-> Persistent Clean
 
-If a Model is not stored in the DataStore but has a key and is associated with
-a Session, it's Pending.
+3. Persistent
 
-Associate raises.
-Flush to Persistent.
+If a Model is stored in the DataStore, it's Persistent.  Its key is COMPLETE.
 
-4. Persistent
+If its serialization is SAME with that in the DataStore, it's Persistent Clean.
+If its serialization is DIFF with that in the DataStore, it's Persistent Dirty.
+If Persistent Dirty model is not marked as deleted, it's Persistent Dirty Put.
+If Persistent Dirty model is marked as deleted, it's Persistent Dirty Delete.
 
-If a Model is stored in the DataStore, has a key and is associated with a
-Session, it's Persistent.
+Persistent Clean --delitem-> Persistent Dirty Delete
+Persistent Clean --modify-> Persistent Dirty Put
+Persistent Dirty Put --delitem-> Persistent Dirty Delete
+Persistent Dirty Put --modify-> Persistent Dirty Put
+Persistent Dirty Put --flush-> Persistent Clean (will be included
+ndb.put_multi)
+Persistent Dirty Delete --getitem-> raise KeyError
+Persistent Dirty Delete --delitem-> raise KeyError
+Persistent Dirty Delete --flush-> Absent (will be included ndb.delete_multi)
 
-Associate raises.
+* Identified/Unidentified
 
-5. Dirty
+If a Model has INCOMPLETE key, it's Unidentified.
+If a Model has COMPLETE key, it's Identified.
 
-If a Model is stored in the DataStore, has a key and is associated with a
-Session but its contents are modified, it's Dirty.
+If a Model is Persistent, it's Identified always.
 
-Associate raises.
-Flush to Persistent.
+* Clean/Dirty
 
-6. Detached
+If a Model has SAME serialization with the stored in the DataStore, it's Clean.
+If a Model has DIFF serialization with the stored in the DataStore, it's Dirty.
 
-If a Model is stored in the DataStore, have a key but is not associated with a
-Session, it's Detached.
+* Pending Put/Delete
 
-Associate to Persistent.
+If a Model is New or Persistent Dirty Put, it's Pendin Put.
+If a Model is Persistent Dirty Delete, it's Pending Delete.
 
+
+Session
+-------
 
 Session does not assign keys, nor care of key contents. (complete,
 string/integer id, etc)
@@ -72,139 +106,250 @@ string/integer id, etc)
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from itertools import ifilter
-from itertools import imap
-
+import logging
 
 from google.appengine.ext import ndb
 from google.appengine.ext.ndb.polymodel import PolyModel
 
 
-class Session(object):
+logger = logging.getLogger(__name__)
 
-    def __init__(self):
-        self.entries = {}
 
-    def __getitem__(self, key):
-        if key in self.entries:
-            entry = self.entries[key]
-            return entry.ndb_model
-
-        ndb_model = key.get()
-        if ndb_model is None:
-            raise KeyError(key)
-        self.associate(ndb_model, stored=True)
-        return ndb_model
-
-    def __setitem__(self, key, ndb_model):
-        entry = Entry(ndb_model, stored=False)
-        assert entry.state == 'free', entry.state
-
-        ndb_model.key = key
-        assert entry.state == 'new', entry.state
-
-        self.entries[key] = entry
-        ndb_model.__session__ = self
-        assert entry.state == 'pending'
-
-    def __delitem__(self, key):
-        if key not in self.entries:
-            raise KeyError(key)
-        self.detach(key)
-
-    def __contains__(self, key):
-        return key in self.entries
-
-    def associate(self, ndb_model, stored):
-        entry = Entry(ndb_model, stored=stored)
-        assert not entry.associated or entry.ndb_model.__session__ is self,\
-            entry.state
-        if stored:
-            assert entry.state in ('detached', 'persistent'), entry.state
-        else:
-            assert 'new' == entry.state, entry.state
-        self.entries[ndb_model.key] = entry
-        ndb_model.__session__ = self
-        return entry.state
-
-    def detach(self, key):
-        entry = self.entries.pop(key, None)
-        if entry is None:
-            return key
-        entry.ndb_model.__session__ = None
-        assert entry.state == 'detached', entry.state
-        return key
-
-    def query(self, q):
-        for ndb_model in q:
-            self.associate(ndb_model, stored=True)
-            yield ndb_model
-
-    def delete_query(self, q):
-        keys = imap(self.detach, q)
-        ndb.delete_multi(keys)
-
-    def flush(self):
-        dirty_entries = ifilter(lambda e: e.state in ('pending', 'dirty'),
-                                self.entries.values())
-        dirty_models = imap(lambda e: e.ndb_model, dirty_entries)
-        keys = ndb.put_multi(dirty_models)
-        entries = imap(lambda k: self.entries[k], keys)
-        map(Entry.onFlush, entries)
-        return keys
+def serialize(ndb_model):
+    if ndb_model is None:
+        return None
+    else:
+        return ndb_model.__getstate__()
 
 
 class Entry(object):
 
-    def __init__(self, ndb_model, stored):
-        self._ndb_model = ndb_model
-        self._serialized = ndb_model.__getstate__()
-        self._stored = stored
-
-    @property
-    def ndb_model(self):
-        return self._ndb_model
+    def __init__(self, ndb_model, stored_serialization):
+        self.ndb_model = ndb_model
+        self.stored_serialization = stored_serialization
+        self.mark_to_delete = False
 
     @property
     def stored(self):
-        return self._stored
+        return self.stored_serialization is not None
 
     @property
-    def haskey(self):
-        key = self._ndb_model.key
-        # it should have "complete" key, i.e. key id is not None
-        return key is not None and key.id() is not None
+    def present(self):
+        return self.ndb_model is not None
 
     @property
-    def associated(self):
-        ''' Associated with a Session '''
-        ndb_model = self.ndb_model
-        session = ndb_model.__session__
-        return session is not None and ndb_model.key in session
+    def absent(self):
+        return not self.stored and self.ndb_model is None
 
     @property
-    def dirty(self):
-        return self._serialized != self._ndb_model.__getstate__()
+    def new(self):
+        return not self.stored and self.ndb_model is not None
+
+    @property
+    def new_identified(self):
+        return (self.new and
+                self.ndb_model.key is not None and
+                self.ndb_model.key.id() is not None)
+
+    @property
+    def new_unidentified(self):
+        return self.new and (self.ndb_model.key is None or
+                             self.ndb_model.key.id() is None)
+
+    @property
+    def persistent(self):
+        return self.stored
+
+    @property
+    def persistent_clean(self):
+        return (self.stored and self.present and
+                serialize(self.ndb_model) == self.stored_serialization)
+
+    @property
+    def persistent_dirty(self):
+        return (self.persistent_dirty_put or
+                self.persistent_dirty_delete)
+
+    @property
+    def persistent_dirty_put(self):
+        return (self.stored and self.present and
+                serialize(self.ndb_model) != self.stored_serialization)
+
+    @property
+    def persistent_dirty_delete(self):
+        return (self.stored and not self.present)
 
     @property
     def state(self):
-        stored = self.stored
-        haskey = self.haskey
-        associated = self.associated
+        if self.absent:
+            return 'absent'
+        if self.new_identified:
+            return 'new-identified'
+        if self.new_unidentified:
+            return 'new-unidentified'
+        if self.persistent_clean:
+            return 'persistent-clean'
+        if self.persistent_dirty_put:
+            return 'persistent-dirty-put'
+        if self.persistent_dirty_delete:
+            return 'persistent-dirty-delete'
+        assert False
 
-        statemap = {}
-        statemap[False, False, False] = 'free'
-        statemap[False, True, False] = 'new'
-        statemap[False, True, True] = 'pending'
-        statemap[True, True, True] = 'persistent'
-        statemap[True, True, False] = 'detached'
-        state = statemap[stored, haskey, associated]
-        if state == 'persistent' and self.dirty:
-            state = 'dirty'
-        return state
+    def onPut(self):
+        self.stored_serialization = serialize(self.ndb_model)
 
-    def onFlush(self):
-        self._serialized = self._ndb_model.__getstate__()
-        self._stored = True
+    def onDelete(self):
+        self.stored_serialization = None
+        self.ndb_model = None
+
+
+class Session(object):
+
+    def __init__(self):
+        self.identified = {}
+        self.unidentified = []
+
+    def __getitem__(self, key):
+        assert key.id() is not None
+
+        if key in self.identified:
+            entry = self.identified[key]
+            if entry.ndb_model is None:
+                assert not entry.present
+                raise KeyError(key)
+            assert entry.present
+            assert entry.ndb_model.key == key
+            return entry.ndb_model
+
+        ndb_model = key.get()
+        entry = Entry(ndb_model, serialize(ndb_model))
+        self.identified[key] = entry
+
+        if ndb_model is None:
+            assert entry.state == 'absent', entry.state
+            raise KeyError(key)
+        else:
+            ndb_model.__session__ = self
+            assert entry.state == 'persistent-clean', entry.state
+            return ndb_model
+
+    def __setitem__(self, key, ndb_model):
+        assert key.id() is not None
+
+        entry = Entry(ndb_model, None)
+        self.identified[key] = entry
+        ndb_model.key = key
+        ndb_model.__session__ = self
+        assert entry.state == 'new-identified', entry.state
+
+    def __delitem__(self, key):
+        assert key.id() is not None
+
+        entry = self.identified.get(key)
+        if entry is None:
+            # unknown
+            entry = Entry(None, True)
+            self.identified[key] = entry
+            assert entry.persistent
+            assert entry.state == 'persistent-dirty-delete', entry.state
+            return
+
+        if entry.ndb_model is None:
+            raise KeyError(key)
+
+        assert entry.new or entry.persistent, entry.state
+        if entry.new:
+            entry.ndb_model.__session__ = None
+            entry.ndb_model = None
+            del self.identified[key]
+        elif entry.persistent:
+            entry.ndb_model.__session__ = None
+            entry.ndb_model = None
+            assert entry.state == 'persistent-dirty-delete', entry.state
+
+    def __contains__(self, key):
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+
+    def add(self, ndb_model):
+        assert ndb_model is not None
+
+        ndb_model.key = ndb.Key(type(ndb_model), None)
+        ndb_model.__session__ = self
+        entry = Entry(ndb_model, None)
+        self.unidentified.append(entry)
+        assert entry.state == 'new-unidentified', entry.state
+        return entry
+
+    def remove(self, ndb_model):
+        assert ndb_model is not None
+
+        entry = None
+        for entry in self.unidentified:
+            if entry.ndb_model is ndb_model:
+                # Found
+                break
+
+        if entry is None:
+            # Not found
+            return
+
+        # it will raise ValueError if not exists
+        self.unidentified.remove(entry)
+
+        assert entry.state == 'new-unidentified', entry.state
+        ndb_model.__session__ = None
+        entry.ndb_model = None
+        assert entry.state == 'absent', entry.state
+        return entry
+
+    def query(self, q):
+        self.flush()
+        for ndb_model in q:
+            key = ndb_model.key
+            entry = Entry(ndb_model, serialize(ndb_model))
+            self.identified[key] = entry
+
+            if ndb_model is None:
+                assert entry.state == 'absent', entry.state
+            else:
+                ndb_model.__session__ = self
+                assert entry.state == 'persistent-clean', entry.state
+            yield ndb_model
+
+    def delete_query(self, q):
+        self.flush()
+        for key in q:
+            try:
+                del self[key]
+            except KeyError:
+                pass
+
+    def flush(self):
+        to_put = filter(lambda e: e.new or e.persistent_dirty_put,
+                        self.identified.values() + self.unidentified)
+        to_put_keys = ndb.put_multi(e.ndb_model for e in to_put)
+        map(Entry.onPut, to_put)
+        assert all(e.state == 'persistent-clean' for e in to_put)
+
+        # move new-unidentified to identified
+        for e in self.unidentified:
+            key = e.ndb_model.key
+            assert key not in self.identified
+            self.identified[key] = e
+        self.unidentified = []
+
+        to_delete = list((k, e) for k, e in self.identified.items()
+                         if e.persistent_dirty_delete)
+        to_delete_keys = list(k for k, e in to_delete)
+        ndb.delete_multi(to_delete_keys)
+        map(Entry.onDelete, (e for k, e in to_delete))
+
+        return set(to_put_keys + to_delete_keys)
 
 
 class Node(PolyModel):
@@ -220,9 +365,18 @@ class Node(PolyModel):
     1) root key is known and the model is stored: root = session[key]
     2) the model is not stored - A) determine key B) associate with a Session
 
-    root = Node()
-    root.key = ndb.Key(Node, 'ROOT')
-    session.associate(root, stored=True)
+        root = Node()
+        root_key = ndb.Key(Node, 'ROOT')
+        session[root_key] = root
+
+    or
+
+        root = Node()
+        session.add(root)
+
+    then
+
+        session.flush()
 
     2. Every nodes in the tree should have a key.
 
--- MYAPP/tests/test_gae_ndb.py
+++ MYAPP/tests/test_gae_ndb.py
@@ -33,62 +33,304 @@ class SessionTest(TestCase):
         from MYAPP.gae.ndb import Session
         return Session()
 
-    def test_associate_free(self):
+    def find_unidentified_entry(self, session, node):
+        for entry in session.unidentified:
+            if entry.ndb_model == node:
+                return entry
+
+    def test_unknown_getitem_absent(self):
+        from google.appengine.ext.ndb import Key
         from MYAPP.gae.ndb import Node
 
         session = self.make_one()
+        key = Key(Node, 'ROOT')
 
-        node = Node()
-        self.assertRaises(AssertionError,
-                          session.associate,
-                          node, stored=False)
+        try:
+            session[key]
+        except KeyError:
+            pass
+        else:
+            assert False, 'KeyError expected'
 
-    def test_associate_new(self):
-        from google.appengine.ext import ndb
+        entry = session.identified[key]
+        self.assertEquals('absent', entry.state)
+
+    def test_unknown_getitem_persistent_clean(self):
+        from google.appengine.ext.ndb import Key
         from MYAPP.gae.ndb import Node
 
-        session = self.make_one()
-
+        key = Key(Node, 'ROOT')
         node = Node()
-        node.key = ndb.Key(Node, 'named')
-        self.assertEquals('pending',
-                          session.associate(node, stored=False))
-
-    def test_associate_pending(self):
-        from google.appengine.ext import ndb
-        from MYAPP.gae.ndb import Node
-
-        session = self.make_one()
-
-        node = Node()
-        node.key = ndb.Key(Node, 'named')
-        assert 'pending' == session.associate(node, stored=False)
-        self.assertRaises(AssertionError,
-                          session.associate,
-                          node, stored=False)
-
-    def test_associate_persistent(self):
-        from google.appengine.ext import ndb
-        from MYAPP.gae.ndb import Node
-
-        session = self.make_one()
-
-        node = Node()
-        node.key = ndb.Key(Node, 'named')
+        node.key = key
         node.put()
-        assert 'persistent' == session.associate(node, stored=True)
-        self.assertRaises(AssertionError,
-                          session.associate,
-                          node, stored=True)
+        session = self.make_one()
 
-    def test_associate_detached(self):
-        from google.appengine.ext import ndb
+        found = session[key]
+
+        entry = session.identified[key]
+        self.assertEquals(node.key, found.key)
+        self.assertEquals('persistent-clean', entry.state)
+
+    def test_unknown_setitem_new_identified(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        key = Key(Node, 'ROOT')
+
+        session[key] = node
+
+        entry = session.identified[key]
+        self.assertEquals('new-identified', entry.state)
+
+    def test_unknown_add_new_unidentified(self):
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+
+        entry = session.add(node)
+
+        self.assertEquals('new-unidentified', entry.state)
+
+    def test_absent_setitem_new_identified(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+        from MYAPP.gae.ndb import Entry
+
+        key = Key(Node, 'ROOT')
+        session = self.make_one()
+        session.identified[key] = Entry(None, None)
+        assert session.identified[key].state == 'absent'
+        node = Node()
+
+        session[key] = node
+
+        entry = session.identified[key]
+        self.assertEquals('new-identified', entry.state)
+
+    def test_new_unidentified_flush_persistent_clean(self):
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        entry = session.add(node)
+        assert entry.state == 'new-unidentified'
+
+        flushed = session.flush()
+
+        self.assertEquals(set([node.key]), flushed)
+        self.assertTrue(node.key in session.identified)
+        self.assertEquals([], session.unidentified)
+        self.assertEquals('persistent-clean', entry.state)
+
+    def test_new_unidentified_remove_unknown(self):
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        entry = session.add(node)
+        assert entry.state == 'new-unidentified'
+
+        session.remove(node)
+
+        self.assertEquals({}, session.identified)
+        self.assertEquals([], session.unidentified)
+
+    def test_new_identitifed_flush_persistent_clean(self):
+        from google.appengine.ext.ndb import Key
         from MYAPP.gae.ndb import Node
 
         session = self.make_one()
 
         node = Node()
-        node.key = ndb.Key(Node, 'named')
-        node.put()
-        self.assertEquals('persistent',
-                          session.associate(node, stored=True))
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        entry = session.identified[node_key]
+        assert entry.state == 'new-identified'
+
+        flushed = session.flush()
+        self.assertEquals(set([node_key]), flushed)
+        self.assertEquals('persistent-clean', entry.state)
+
+    def test_new_identified_delitem_unknown(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+
+        node = Node()
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        entry = session.identified[node_key]
+        assert entry.state == 'new-identified'
+
+        del session[node_key]
+
+        self.assertEquals({}, session.identified)
+        self.assertEquals([], session.unidentified)
+
+    def test_persistent_clean_delitem_persistent_dirty_delete(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        entry = session.identified[node_key]
+
+        del session[node.key]
+
+        self.assertEquals('persistent-dirty-delete', entry.state)
+
+    def test_persistent_clean_modify_persistent_dirty_put(self):
+        from google.appengine.ext.ndb import Key
+        from google.appengine.ext.ndb import StringProperty
+        from MYAPP.gae.ndb import Node
+
+        class Content(Node):
+            title = StringProperty()
+
+        session = self.make_one()
+        node = Content(title='hello')
+        node_key = Key(Content, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+
+        node.title = 'world'
+
+        entry = session.identified[node_key]
+        self.assertEquals('persistent-dirty-put', entry.state)
+
+    def test_persistent_dirty_put_delitem_persistent_dirty_delete(self):
+        from google.appengine.ext.ndb import Key
+        from google.appengine.ext.ndb import StringProperty
+        from MYAPP.gae.ndb import Node
+
+        class Content(Node):
+            title = StringProperty()
+
+        session = self.make_one()
+        node = Content()
+        node_key = Key(Content, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        node.title = 'hello'
+        entry = session.identified[node_key]
+        assert entry.state == 'persistent-dirty-put'
+
+        del session[node_key]
+
+        self.assertEquals('persistent-dirty-delete', entry.state)
+
+    def test_persistent_dirty_put_modify_persistent_dirty_put(self):
+        from google.appengine.ext.ndb import Key
+        from google.appengine.ext.ndb import StringProperty
+        from MYAPP.gae.ndb import Node
+
+        class Content(Node):
+            title = StringProperty()
+
+        session = self.make_one()
+        node = Content(title='hello')
+        node_key = Key(Content, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        node.title = 'world'
+        entry = session.identified[node_key]
+        assert entry.state == 'persistent-dirty-put'
+
+        node.title = 'foobar'
+
+        self.assertEquals('persistent-dirty-put', entry.state)
+
+    def test_persistent_dirty_put_flush_persistent_clean(self):
+        from google.appengine.ext.ndb import Key
+        from google.appengine.ext.ndb import StringProperty
+        from MYAPP.gae.ndb import Node
+
+        class Content(Node):
+            title = StringProperty()
+
+        session = self.make_one()
+        node = Content(title='hello')
+        node_key = Key(Content, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        node.title = 'world'
+        entry = session.identified[node_key]
+        assert entry.state == 'persistent-dirty-put'
+
+        session.flush()
+
+        self.assertEquals('persistent-clean', entry.state)
+
+    def test_persistent_dirty_delete_getitem_raise_keyerror(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        entry = session.identified[node_key]
+        del session[node.key]
+        assert entry.state == 'persistent-dirty-delete'
+
+        try:
+            session[node.key]
+        except KeyError:
+            pass
+        else:
+            assert False, 'KeyError expected'
+
+    def test_persistent_dirty_delete_delitem_raise_keyerror(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        entry = session.identified[node_key]
+        del session[node.key]
+        assert entry.state == 'persistent-dirty-delete'
+
+        try:
+            del session[node.key]
+        except KeyError:
+            pass
+        else:
+            assert False, 'KeyError expected'
+
+    def test_persistent_dirty_delete_flush_absent(self):
+        from google.appengine.ext.ndb import Key
+        from MYAPP.gae.ndb import Node
+
+        session = self.make_one()
+        node = Node()
+        node_key = Key(Node, 'ROOT')
+        session[node_key] = node
+        session.flush()
+        assert session.identified[node_key].state == 'persistent-clean'
+        entry = session.identified[node_key]
+        del session[node.key]
+        assert entry.state == 'persistent-dirty-delete'
+
+        flushed = session.flush()
+
+        self.assertTrue(node_key in flushed)
+        self.assertTrue(node_key in session.identified)
+        self.assertEquals('absent', entry.state)
